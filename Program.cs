ee1aac866046fee69f4f588eaa594ce8dc9719e8
sneg <sneg.sbg@gmail.com>
2023-01-15 15:20:08 +0300

    min_and_max_in_3

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// a=2, b=3, c=7
+int number_1 = 2;
+int number_2 = 3;
+int number_3 = 7;
+int max = number_1;
+if (number_2 > max) max = number_2;
+if (number_3 > max) max = number_3;
+Console.WriteLine($"Максимальное из трех чисел {max}");
+// a=44, b=5, c=78
+int number_4 = 44;
+int number_5 = 5;
+int number_6 = 78;
+int max1 = number_4;
+if (number_5 > max) max = number_5;
+if (number_6 > max) max = number_6;
+Console.WriteLine($"Максимальное из трех чисел {max1}");
+// a=22, b=3, c=9
+int number_7 = 22;
+int number_8 = 3;
+int number_9 = 9;
+int max2 = number_7;
+if (number_8 > max) max = number_8;
+if (number_9 > max) max = number_9;
+Console.WriteLine($"Максимальное из трех чисел {max2}");
